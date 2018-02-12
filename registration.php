8ac81de39b105f9c27f99d5cb341009c0b453b5a
Ashwini <ashwinigupta4455@gmail.com>
2018-02-12 11:30:58 +0530

    Reformatting the Code.

--- registration.php
+++ registration.php
@@ -52,45 +52,47 @@ require('layout/header.php');
         } else {
             echo "Error Occur!";
         }
-    }else{
+    } else
+        {
 ?>
             <div class="col-md-6 offset-3">
-                <h3 class="text-center"><?php echo $role?> Registration</h3>
-                <hr>
-                <?php
-                    if ($role == 'User'){
-                        echo '<div class="alert alert-info" role="alert">
-                                  <strong>Hospital!</strong> follow this <a href="registration.php?id=Hospital">link</a>.
-                              </div>';
+        <h3 class="text-center"><?php echo $role?> Registration</h3>
+        <hr>
+        <?php
+            if ($role == 'User'){
+                echo '<div class="alert alert-info" role="alert">
+                          <strong>Hospital!</strong> follow this <a href="registration.php?id=Hospital">link</a>.
+                      </div>';
+            }
+        ?>
+        <div class="form">
+            <form name="registration" action="" method="post">
+                <input class="form-control" type="text" name="name" placeholder="<?php if ($role == 'Hospital'){ echo 'Hospital Name'; } else { echo 'Name'; }?>" required />
+                <?php if ($role == 'User'){
+                        echo '<label for="blood_group">Select Blood Group</label>
+                                <select class="form-control" id="blood_group" name="blood_group" required>
+                                    <option value="Select">-----Select-----</option>
+                                    <option value="A+">A+</option>
+                                    <option value="A-">A-</option>
+                                    <option value="B+">B+</option>
+                                    <option value="B-">B-</option>
+                                    <option value="AB+">AB+</option>
+                                    <option value="AB-">AB-</option>
+                                    <option value="O+">O+</option>
+                                    <option value="O-">O-</option>
+                                </select>';
                     }
                 ?>
-                <div class="form">
-                            <form name="registration" action="" method="post">
-                                <input class="form-control" type="text" name="name" placeholder="<?php if ($role == 'Hospital'){ echo 'Hospital Name'; } else { echo 'Name'; }?>" required />
-                                <?php if ($role == 'User'){
-                                        echo '<label for="blood_group">Select Blood Group</label>
-                                                <select class="form-control" id="blood_group" name="blood_group" required>
-                                                    <option value="Select">-----Select-----</option>
-                                                    <option value="A+">A+</option>
-                                                    <option value="A-">A-</option>
-                                                    <option value="B+">B+</option>
-                                                    <option value="B-">B-</option>
-                                                    <option value="AB+">AB+</option>
-                                                    <option value="AB-">AB-</option>
-                                                    <option value="O+">O+</option>
-                                                    <option value="O-">O-</option>
-                                                </select>';
-                                    }
-                                ?>
-                                <input class="form-control" type="text" name="username" placeholder="Username" required />
-                                <input class="form-control" type="email" name="email" placeholder="Email" required />
-                                <input class="form-control" type="password" name="password" placeholder="Password" required />
-                                <input class="btn btn-primary btn-block" type="submit" name="submit" value="Register" />
-                            </form>
-                        </div>
-            </div>
+                <input class="form-control" type="text" name="username" placeholder="Username" required />
+                <input class="form-control" type="email" name="email" placeholder="Email" required />
+                <input class="form-control" type="password" name="password" placeholder="Password" required />
+                <input class="btn btn-primary btn-block" type="submit" name="submit" value="Register" />
+            </form>
+        </div>
+    </div>
 
-<?php }
+<?php
+        }
 
     //include header template
     require('../layout/footer.php');
